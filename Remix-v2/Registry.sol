6b0251bb8277637a410574e8aea7ee3b02eab0fe
Dev Bharel <dev@global.id>
2021-08-04 16:15:13 -0400

    stuff

--- Remix-v2/Registry.sol
+++ Remix-v2/Registry.sol
@@ -10,10 +10,10 @@ contract Registry {
         string id;
         address nftcontract;
     }
-    struct ThemeRecord {
+    struct SystemRecord {
         address publisher;
     }
-    struct SystemRecord {
+    struct ThemeRecord {
         address locationNFTContract;
         address questNFTContract;
         address regionsNFTContract;
@@ -34,8 +34,8 @@ contract Registry {
         return name.namehash();
     }
     
-    function registerSystem(bytes memory name) public {
-        bytes32 nh = name.namehash();
+    function registerSystem(string memory name) public {
+        bytes32 nh = bytes(name).namehash();
         namehashToType[nh] = 'system';
         systemRecords[nh] = SystemRecord(address(0x0), address(0x0), address(0x0), address(0x0), address(0x0), address(0x0));
     }
@@ -51,47 +51,37 @@ contract Registry {
         return keccak256(abi.encodePacked((s1))) == keccak256(abi.encodePacked((s2)));
     }
     
-    function resolveSystemNamehash(bytes memory name) public view returns (SystemRecord memory) {
-        bytes32 nh = name.namehash();
+    function resolveSystemNamehash(string memory name) public view returns (SystemRecord memory) {
+        bytes32 nh = bytes(name).namehash();
         return systemRecords[nh];  
     }
     
-    function resolveThemeNamehash(bytes memory name) public view returns (ThemeRecord memory) {
-        bytes32 nh = name.namehash();
+    function resolveThemeNamehash(string memory name) public view returns (ThemeRecord memory) {
+        bytes32 nh = bytes(name).namehash();
         return themeRecords[nh];  
     }
     
-    function resolveNodeNamehash(bytes memory name) public view returns (NodeRecord memory) {
-        bytes32 nh = name.namehash();
+    function resolveNodeNamehash(string memory name) public view returns (NodeRecord memory) {
+        bytes32 nh = bytes(name).namehash();
         return nodeRecords[nh];
     }
 
-    function registerTheme(bytes memory name, address pubAddress) public {
-      bytes32 nh = name.namehash();
+    function registerTheme(string memory name, address pubAddress) public payable {
+      require(msg.value == 1 ether, "It costs 1 ETH to purchase a Universe");
+      bytes32 nh = bytes(name).namehash();
       namehashToType[nh] = "theme";
       themeRecords[nh] = ThemeRecord(pubAddress);
     }
 
-    function updateTheme(bytes memory name, address pubAddress) public {
-      bytes32 nh = name.namehash();
+    function updateTheme(string memory name, address pubAddress) public {
+      bytes32 nh = bytes(name).namehash();
       themeRecords[nh].publisher = pubAddress;
     }
 
-    function resolveNamehashType(bytes memory name) public returns (string memory) {
-      return namehashToType[name.namehash()];
+    function resolveNamehashType(string memory name) public returns (string memory) {
+      return namehashToType[bytes(name).namehash()];
     }
 
 
-    /*
-    function resolveNamehash(bytes memory name) public view returns (address) {
-        //always resolve to a resolver contract, which will return appropriate data 
-        // for nft.nftcontract all the nfts should be resolved to the nftcontract address and let that address return information about the nft
-        return contracts[name.namehash()];
-    }
-    
-    function setNamehashContract(bytes memory name, address contractAddress) public {
-        contracts[name.namehash()] = contractAddress;
-    } 
-    */
     
 }
